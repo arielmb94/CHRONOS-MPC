012955b0e4d35c3b10f93f1c42daa3c2a5783ab5
Ariel Medero <arielmedero@mbp-de-ariel.home>
2025-03-24 22:26:34 +0100

    stirring tank: more interesting reference

--- examples/Stirring Tank Reactor/stirring_tank_sim_lpv.m
+++ examples/Stirring Tank Reactor/stirring_tank_sim_lpv.m
@@ -5,18 +5,19 @@ clear c_dat v_dat uk rk
 ck = x_prev(1);
 vk = x_prev(2);
 
-r_c = 0.65;
-r_v = 0.53;
-
-f_c = ck;
-f_v = vk;
-tau = 15;
 
 t1 = cputime;
 for i = 1:2400
 
-f_c = f_c + Ts*(-f_c/tau+r_c/tau);
-f_v = f_v + Ts*(-f_v/tau+r_v/tau);
+if i < 900
+    ref_c = 0.27 + (0.65-0.27)*i/900;
+elseif i >= 900 && i<1800
+    ref_c = 0.65;
+else
+    ref_c = 0.65 - (0.65-0.3)*(i-1800)/600;
+end
+
+ref_v = - M/(log(1/(theta_f*k*ref_c)*(1-ref_c)));
 
 % Update LPV model
 A_lpv = eye(2)+Ts*[-1/theta_f-k*exp(-M/vk) -k*ck*M*exp(-M/vk)/(vk^2);
@@ -30,7 +31,7 @@ mpc = update_mpc_sys_dynamics(mpc,A_lpv,B_lpv,Bd_lpv);
 
 d = [1;vk];
 
-xref = [f_c;f_v];
+xref = [ref_c;ref_v];
 
 [u_prev,J,x0] = mpc_solve(x0,x_prev,u_prev,xref,d,mpc,[],[],[]);
 
@@ -43,7 +44,7 @@ c_dat(:,i) = ck;
 v_dat(:,i) = vk;
 Jk(i) = J;
 uk(i) = u_prev;
-rk(i) = f_c;
+rk(i) = ref_c;
 
 end
 t2 = cputime-t1;
