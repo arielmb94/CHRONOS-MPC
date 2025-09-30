c4df7c6474fa093ad46d3abb9fd333f47c4ea7ee
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2025-09-30 09:28:20 +0200

    adapt gradient computation function to codegen rules

--- solver/grad_f0_MPC.m
+++ solver/grad_f0_MPC.m
@@ -3,19 +3,19 @@ function grad_J = grad_f0_MPC(mpc,err,deltaU,U,grad_ter,z)
     grad_J = zeros(mpc.Nx+mpc.Nu,1);
 
     if ~isempty(mpc.gradErrQe)
-        grad_J = grad_J - mpc.gradErrQe*err;
+        grad_J(:) = grad_J - mpc.gradErrQe*err;
     end    
 
     if ~isempty(mpc.gradDiffCtlrRdu)
-        grad_J = grad_J + mpc.gradDiffCtlrRdu*deltaU;
+        grad_J(:) = grad_J + mpc.gradDiffCtlrRdu*deltaU;
     end  
 
     if ~isempty(mpc.gradCtlrRu)
-        grad_J = grad_J + mpc.gradCtlrRu*U;
+        grad_J(:) = grad_J + mpc.gradCtlrRu*U;
     end
 
     if ~isempty(mpc.gradCtlrru)
-        grad_J = grad_J + mpc.gradCtlrru;
+        grad_J(:) = grad_J + mpc.gradCtlrru;
     end
 
     if mpc.ter_ingredients
@@ -23,11 +23,11 @@ function grad_J = grad_f0_MPC(mpc,err,deltaU,U,grad_ter,z)
     end  
 
     if ~isempty(mpc.gradPerfQz)
-        grad_J = grad_J + mpc.gradPerfQz*z;
+        grad_J(:) = grad_J + mpc.gradPerfQz*z;
     end  
 
     if ~isempty(mpc.gradPerfqz)
-        grad_J = grad_J + mpc.gradPerfqz;
+        grad_J(:) = grad_J + mpc.gradPerfqz;
     end
 
 end
