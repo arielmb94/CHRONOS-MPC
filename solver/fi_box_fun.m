9ddbb00a0807586d5691660b53d7942991636765
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2025-10-28 10:50:35 +0100

    added logic to detect active inequality conditions

--- solver/fi_box_fun.m
+++ solver/fi_box_fun.m
@@ -2,10 +2,24 @@ function cnstr = fi_box_fun(cnstr,x,N,n,v_feas)
 
 if ~isempty(cnstr.min)
 
+    % If active-set optimization enabled reset indicator vector
+    if cnstr.min_activ_set
+        cnstr.min_activ_indicator(:) = 0;
+    end
+
     for k = 1:N/n
 
         cnstr.fi_min_x0((k-1)*n+1:k*n) = cnstr.min-x((k-1)*n+1:k*n);
 
+        % check wich constraints are active
+        if cnstr.min_activ_set
+
+            % check wich conditions enabled for the k horizon step
+            step_indicator = x((k-1)*n+1:k*n) < cnstr.min_activ_lim;
+            % update the active condition indicator vector
+            cnstr.min_activ_indicator = cnstr.min_activ_indicator | step_indicator;
+
+        end
     end
 
     % for feasibility solver: fi - v_feas <= 0
@@ -17,10 +31,24 @@ end
 
 if ~isempty(cnstr.max)
 
+    % If active-set optimization enabled reset indicator vector
+    if cnstr.max_activ_set
+        cnstr.max_activ_indicator(:) = 0;
+    end
+
     for k = 1:N/n
 
         cnstr.fi_max_x0((k-1)*n+1:k*n) = x((k-1)*n+1:k*n)-cnstr.max;
 
+        % check wich constraints are active
+        if cnstr.max_activ_set
+
+            % check wich conditions enabled for the k horizon step
+            step_indicator = x((k-1)*n+1:k*n) > cnstr.max_activ_lim;
+            % update the active condition indicator vector
+            cnstr.max_activ_indicator = cnstr.max_activ_indicator | step_indicator;
+
+        end
     end
 
     % for feasibility solver: fi - v_feas <= 0
