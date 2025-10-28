2a39e1584de81e2bf0125caf6134ae8228e93986
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2025-10-28 11:25:59 +0100

    added logic to avoid computing gradient and hessian inactive conditions

--- solver/grad_box_Ind.m
+++ solver/grad_box_Ind.m
@@ -1,12 +1,23 @@
-function grad_Ind_x0 = grad_box_Ind(fi,grad_fi)
+% n_i is size of
+function grad_Ind_x0 = grad_box_Ind(fi,grad_fi,...
+                                    active_set,active_indicator,n_i)
 
     [n,m] = size(grad_fi);
 
     grad_Ind_x0 = zeros(n,1);
     for i = 1:m
 
-        grad_Ind_x0 = grad_Ind_x0 - grad_fi(:,i)/fi(i);
-
+        if active_set
+            index = mod(m,n_i);
+            if ~index
+                index = n_i;
+            end
+            if active_indicator(index)
+                grad_Ind_x0 = grad_Ind_x0 - grad_fi(:,i)/fi(i);
+            end
+        else
+            grad_Ind_x0 = grad_Ind_x0 - grad_fi(:,i)/fi(i);
+        end
     end
 
 end
--- solver/hess_linear_Ind.m
+++ solver/hess_linear_Ind.m
@@ -1,4 +1,5 @@
-function hess = hess_linear_Ind(fi,hess_fi)
+function hess = hess_linear_Ind(fi,hess_fi,...
+                                active_set,active_indicator,n_i)
 
     % Hess(Phi) = sum((grad(fi)*grad(fi)^T)/fi^2) 
   
@@ -7,9 +8,18 @@ function hess = hess_linear_Ind(fi,hess_fi)
 
     hess = zeros(n);
     for i = 1:m
-
-        hess = hess + hess_fi{i}/(fi(i)^2);
-
+        
+        if active_set
+            index = mod(m,n_i);
+            if ~index
+                index = n_i;
+            end
+            if active_indicator(index)
+                hess = hess + hess_fi{i}/(fi(i)^2);
+            end
+        else
+            hess = hess + hess_fi{i}/(fi(i)^2);
+        end
     end
 
 end
