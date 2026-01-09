fb63620729b1a35178b435330d57d9661ebdba18
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2026-01-09 10:17:02 +0100

    Working simulink with lateral MPC controller

---
+++ examples/SAV/SimpleSim/sav_lat_controller_init.m
@@ -0,0 +1,171 @@
+%%% Load controllers and sampling time
+
+load('xmotor.mat')
+xmotor = x;
+
+La_l = xmotor(1);
+Ra_l = xmotor(2);
+K_l = xmotor(3);
+J_l = xmotor(4);
+F_l = xmotor(5);
+
+La_r = xmotor(1)
+Ra_r = xmotor(2)
+K_r = xmotor(3)
+J_r = xmotor(4)
+F_r = xmotor(5)
+
+tau_m = xmotor(6)
+
+load('xNL.mat')
+
+C_sigma_2 = x(1)
+C_sigma_1 = x(2)
+C_sigma_0 = x(3)
+C_alpha_f2 = x(4)
+C_alpha_f1 = x(5)
+C_alpha_f0 = x(6)
+C_alpha_r2 = x(7)
+C_alpha_r1 = x(8)
+C_alpha_r0 = x(9)
+eps = x(10)
+wn = x(11)
+tau = x(12)
+Iz = x(13)
+R = x(14)
+
+servoSIM = ss([0 1; -wn^2 -2*eps*wn],[0; wn^2],[1 0],0);
+servoSIM.InputDelay = tau;
+
+Ts=1/50;
+
+Observer_Motor_PI
+
+Servo = ss([0 1; -wn^2 -2*eps*wn],[0; wn^2],[1 0],0);
+d = 9; %tau/Ts
+z = tf('z',Ts);
+delay = ss(z^-d);
+
+Servo = series(delay,c2d(Servo,Ts));
+
+load("controllers.mat")
+
+%% Car Parameters
+
+m=1.1934; % mass of the vehicle
+l_r=0.1049; % distance from center of gravity to rear wheel
+L=0.174;
+l_f=L-l_r; 
+
+%% Create MPC object
+
+N = 35;         % Prediction Horizon
+N_h_ctr = 25;    % Control Horizon
+
+% Create mpc struct
+mpc = init_mpc(N,N_h_ctr);
+%% LTI system
+
+% Init speed
+vx = 1;
+
+C_f = C_alpha_f2*vx^2+C_alpha_f1*vx+C_alpha_f0;
+C_r = C_alpha_r2*vx^2+C_alpha_r1*vx+C_alpha_r0;
+
+A = update_BM(vx);
+B = [C_f/m; C_f*l_f/Iz];
+C = [0 1];
+
+
+% Initialize system dynamics
+% System discretized with forward Euler discretization:
+% x+ = (I+Ts*A)*x+Ts*B*u+Ts*Bd*d
+mpc = init_mpc_system(mpc,eye(2)+Ts*A,Ts*B,0,C,0,0);
+
+%% Constraints
+
+% State constraints
+x_min = 0.01*ones(mpc.nx,1);
+x_max = 1*ones(mpc.nx,1);
+%mpc = init_mpc_state_cnstr(mpc,x_min,x_max);
+
+% State constraints only on terminal states
+x_ter_min = [];
+x_ter_max = [];
+%mpc = init_mpc_ter_state_cnstr(mpc,x_ter_min,x_ter_max);
+
+% Control input constraints
+u_min = -0.7*ones(mpc.nu,1);
+u_max = 0.7*ones(mpc.nu,1);
+mpc = init_mpc_u_cnstr(mpc,u_min,u_max);
+
+% Control inputs variation constraints
+du_min = -0.1*ones(mpc.nu,1);
+du_max = 0.1*ones(mpc.nu,1);
+%mpc = init_mpc_delta_u_cnstr(mpc,du_min,du_max);
+
+% Output constraints
+y_min = [];
+y_max = [];
+%mpc = init_mpc_output_cnstr(mpc,y_min,y_max);
+
+%% General Linear Inequalities
+
+% General Linear Inequalities are defined as:
+% yh = Ch*x+Dh*u+Ddh*di
+Ch = [];
+Dh = [];
+Ddh = [];
+
+h_min = [];
+h_max = [];
+
+%mpc = init_mpc_lin_custom_cnstr(mpc,h_min,h_max,Ch,Dh,Ddh);
+
+%% Terminal Ingredients
+
+% Terminal ingredients are computed using the dLQR method
+Qx = diag([30 30]);         % State Penalty
+Ru = 1;                     % Control Penalty
+ter_constraint = 0;         % Only terminal cost
+x_ref_is_y = 0;             % The terminal reference cannnot be extracted 
+                            % from the mpc tracking reference
+
+% Initialize terminal ingredients using the dLQR method                           
+%[mpc] = init_mpc_ter_ingredients_dlqr(mpc,Qx,Ru,ter_constraint,x_ref_is_y);
+
+%% Costs
+
+% Tracking penalty
+Qe = diag(50*ones(mpc.ny,1));
+mpc = init_mpc_Tracking_cost(mpc,Qe);
+
+% Control inputs variation penalty
+Rdu = 10;
+mpc = init_mpc_DiffControl_cost(mpc,Rdu);
+
+% Control penalty
+Ru = 5;    % Quadratic penalty on control action u'*Ru*u
+ru = [];    % Linear penalty on control action vector: ru'*u 
+mpc = init_mpc_Control_cost(mpc,Ru,ru);
+
+%% Performance Cost Matrix
+
+% Performance Vector are defined as:
+% z = Cz*x+Dz*u+Ddz*dz
+Cz = [];
+Dz = [];
+Ddz = [];
+
+Qz = [];    % Quadratic penalty on performance vector: z'*Qz*z
+qz = [];    % Linear penalty on performance vector: qz'*z 
+
+% Init performance cost
+%mpc = init_mpc_Lin_Custom_cost(mpc,Cz,Dz,Ddz,Qz,qz);
+
+%% Init conditions for simulation
+
+% use warm start function to get optimization vector initial value
+x_prev = [0; 0];
+u_prev = 0;
+x0 = init_mpc_warm_start(mpc,x_prev,u_prev);
---
+++ examples/SAV/sav_lat_mpc_solve.m
@@ -0,0 +1,13 @@
+function [u,x0] = sav_lat_mpc_solve(mpc,x0,vx,vy,yaw_rate,yaw_rate_ref,...
+                                                    u_prev)
+
+
+A = update_BM(vx);
+Ak = eye(2)+0.0200*A;
+mpc = update_mpc_sys_dynamics(mpc,Ak,[],[]);
+
+x_prev = [vy;yaw_rate];
+
+[u,x0] = mpc_solve(mpc,x0,x_prev,u_prev,yaw_rate_ref,[],[],[],[]);
+
+end
