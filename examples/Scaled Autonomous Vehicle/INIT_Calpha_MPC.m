7f2405e42d53db7c7412bb1bc0c491270c08c080
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2026-01-23 17:13:54 +0100

    make plots great again

--- examples/Scaled Autonomous Vehicle/INIT_Calpha_MPC.m
+++ examples/Scaled Autonomous Vehicle/INIT_Calpha_MPC.m
@@ -1,7 +1,12 @@
 %%% Load controllers and sampling time
+
 clear all
 close all
 
+% Sampling Time
+Ts=1/50;
+
+% Motor Parameters
 load('xmotor.mat')
 xmotor = x;
 
@@ -11,53 +16,49 @@ K_l = xmotor(3);
 J_l = xmotor(4);
 F_l = xmotor(5);
 
-La_r = xmotor(1)
-Ra_r = xmotor(2)
-K_r = xmotor(3)
-J_r = xmotor(4)
-F_r = xmotor(5)
+La_r = xmotor(1);
+Ra_r = xmotor(2);
+K_r = xmotor(3);
+J_r = xmotor(4);
+F_r = xmotor(5);
 
-tau_m = xmotor(6)
+tau_m = xmotor(6);
 
+% SAV parameters
 load('xNL.mat')
 
-C_sigma_2 = x(1)
-C_sigma_1 = x(2)
-C_sigma_0 = x(3)
-C_alpha_f2 = x(4)
-C_alpha_f1 = x(5)
-C_alpha_f0 = x(6)
-C_alpha_r2 = x(7)
-C_alpha_r1 = x(8)
-C_alpha_r0 = x(9)
-eps = x(10)
-wn = x(11)
-tau = x(12)
-Iz = x(13)
-R = x(14)
+C_sigma_2 = x(1);
+C_sigma_1 = x(2);
+C_sigma_0 = x(3);
+C_alpha_f2 = x(4);
+C_alpha_f1 = x(5);
+C_alpha_f0 = x(6);
+C_alpha_r2 = x(7);
+C_alpha_r1 = x(8);
+C_alpha_r0 = x(9);
+eps = x(10);
+wn = x(11);
+tau = x(12);
+Iz = x(13);
+R = x(14);
 
+% Steering Actuator Model
 servoSIM = ss([0 1; -wn^2 -2*eps*wn],[0; wn^2],[1 0],0);
 servoSIM.InputDelay = tau;
 
-Ts=1/50;
-
+% Disturbance Observer for the Motor Control
 Init_Observer_Motor_PI
 
-Servo = ss([0 1; -wn^2 -2*eps*wn],[0; wn^2],[1 0],0);
-d = 9; %tau/Ts
-z = tf('z',Ts);
-delay = ss(z^-d);
-
-Servo = series(delay,c2d(Servo,Ts));
-
+% Longitudinal and Motor Controllers
 load("controllers.mat")
 
+% Init MPC for the lateral controller
 sav_lat_controller_init
-%% Map
+%% Load Map
 
 Cartoon
 
-%% Inital conditions
+%% Inital conditions for Simulation
 
 global pathindex
 pathindex = 1;
@@ -73,20 +74,34 @@ vref = 1.25;
 %% Run simulation and plot trajectory
 pathindex = 1;
 
-
 out = sim('CAR_SIM_MPC.slx',100);
+
+%% Plots
+
 close all
 figure
 plot(Track.X,Track.Y)
 hold on
 plot(out.X,out.Y)
 axis equal
-
-%%
-figure
-plot3(Track.X,Track.Y,ones(1,length(Track.X)))
-hold on
-plot3(Track.X,Track.Y,1.45*ones(1,length(Track.X)))
-plot3(out.X,out.Y,out.Vx) 
-axis equal
 grid on
+legend('Desired Trajectory', 'Vehicle Trajectory')
+xlabel('X [m]')
+ylabel('Y [m]')
+
+figure
+subplot(2,1,1)
+plot(out.time,out.YawRateRef,out.time,out.YawRate)
+grid on
+legend('Reference Yaw Rate', 'Vehicle Yaw Rate')
+xlabel('Time [s]')
+ylabel('Yaw Rate [rad/s]')
+
+subplot(2,1,2)
+plot(out.time,out.deltaK)
+grid on
+legend('Steering Command')
+xlabel('Time [s]')
+ylabel('Steering Angle [deg]')
+
+
