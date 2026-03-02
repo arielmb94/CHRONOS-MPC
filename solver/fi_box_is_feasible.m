29ecefbf19e94d5e0bab4d1bd4172aedd7028427
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2026-03-02 09:52:24 +0100

    added new simplified check feasibility functions

--- solver/fi_box_is_feasible.m
+++ solver/fi_box_is_feasible.m
@@ -1,8 +1,19 @@
 function feas = fi_box_is_feasible(cnstr)
 
+    feas = 1;
     if any(cnstr.fi_min_x0>0) || any(cnstr.fi_max_x0>0)
         feas = 0;
-    else
-        feas = 1;
     end
+
+    if feas && cnstr.min_slack_nv
+        if any(cnstr.min_slack_positivity_fi_x0>0)
+            feas = 0;
+        end
+    end
+    if feas && cnstr.max_slack_nv
+        if any(cnstr.max_slack_positivity_fi_x0>0)
+            feas = 0;
+        end
+    end
+
 end
