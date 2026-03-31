fe9a1e959eb34edb5da9c8e8387f0dbbff453d80
viniciusmorenosanches <viniciuseislaine@gmail.com>
2026-03-31 18:32:54 -0300

    Revert "Fixing functions for mex"
    This reverts commit 097def04b74343061d024fddd1fc80e16685761b.

--- solver/ter_set_feas_Ind_fun.m
+++ solver/ter_set_feas_Ind_fun.m
@@ -3,12 +3,8 @@ function [grad_ter_Ind_x0,hess_ter_Ind_x0] = ...
 % optimization variables are stored as [u0 x1 u1 x2 ... xn un xn+1 v]^T
 % v is feasibility problem slack variable
 
-% compute error separately (for mex purposes)
-x_err = x_ref - s_ter;
-x_err = x_err(:);
-
 % Gradient of Terminal Set Constraint
-grad_ter = [2*P*x_err;1]; % grad_ter = [-I*P*x_err;-1] = -[P*x_err;1] 
+grad_ter = [2*P*(x_ref-s_ter);1]; % grad_ter = [-I*P*x_err;-1] = -[P*x_err;1] 
 
 % Gradient of Indicator Function for Terminal Set Constraint
 grad_ter_Ind_x0 = zeros(n,1);
@@ -16,7 +12,7 @@ grad_ter_Ind_x0 = zeros(n,1);
 % Negative of Indicator gradient cancels with negative of terminal ser
 % gradient
 % - -[P*x_err;1]/fi_ter_x0 = grad_ter/fi_ter_x0
-grad_ter_Ind_x0(end-(nx) : end) = grad_ter./fi_ter_x0;
+grad_ter_Ind_x0(end-(nx) : end) = grad_ter/fi_ter_x0;
 
 % Hessian of Indicator Function for Terminal Set Constraint
 hess_ter_Ind_x0 = zeros(n);
@@ -25,6 +21,6 @@ Hess_fi_ter = zeros(nx+1);
 Hess_fi_ter(1:nx,1:nx) = 2*P;
 
 hess_ter_Ind_x0(end-(nx) : end, end-(nx) : end) = ...
-    (grad_ter*grad_ter')./(fi_ter_x0^2) - Hess_fi_ter./fi_ter_x0;
+    (grad_ter*grad_ter')/(fi_ter_x0^2) - Hess_fi_ter/fi_ter_x0;
 
 end
